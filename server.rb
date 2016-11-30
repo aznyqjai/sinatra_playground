610ea36cc84d84ee10eae664722e0db9999537a1
aznyqjai <aznyqjai@gmail.com>
2016-11-30 15:58:27 -0500

    plenty done

---
+++ server.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+
+#configure sinatra public folder
+set :public_folder, File.dirname(__FILE__) + "/public"
+get '/' do
+  @q = params[:q]
+  @current_user = "QQ"
+  erb :home
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/sup' do
+  puts "THESE ARE MY PARAMS"
+  puts params.inspect
+  puts params[:hi]   
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/sign-in' do
+  puts "sign-in"
+  puts "your params are " + params.inspect
+end
+
+get '/happy' do
+  erb :happy
+end
+
+get '/anotherpage' do
+  erb :anotherpage
+end
+
+get '/test' do
+  haml :test
+end
+
+get '/contact' do
+  erb :contact
+end
+
+post '/contact_us' do
+  puts "emailing"
+  puts "your params are " + params.inspect
+end
+
+
+
